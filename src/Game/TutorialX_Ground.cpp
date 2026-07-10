c9c6621ab41b3bf75ef3a9cf9ed05c5e69b3488d
attakapoteto-admin <great.fujii.kouuuki123@gmail.com>
2026-07-11 03:11:53 +0900

    やっとオリジナルのマップとマップマテリアルを当てはめることに成功しました。

--- src/Game/TutorialX_Ground.cpp
+++ src/Game/TutorialX_Ground.cpp
@@ -16,7 +16,7 @@ bool Ground::Init()
     SetName("Ground");
 
     //auto ground = Scene::Object::Create<Object>("Ground");
-    AddComponent<ComponentModel>("data/Sample/SwordBout/Stage/Stage00.mv1");
+    AddComponent<ComponentModel>("data/Sample/SwordBout/Stage/Stage.mv1");
     AddComponent<ComponentCollisionModel>()->AttachToModel();
 
     return true;
