70ce6769490361bc9966f9f375f238fb1f5fbfd0
504-40\cre <a>
2026-08-29 18:49:40 +0900

    簡単にだけどタイトルを追加しました。

---
+++ src/Game/Title.h
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------//
+//!	@file	Title.h
+//! @brief	Title
+//---------------------------------------------------------------------------//
+#pragma once
+#include <System/Scene.h>
+
+namespace PoittersPoint {
+
+class Title : public Scene::Base
+{
+public:
+    BP_CLASS_DECL(Title, u8"Title");
+
+    //! @brief 初期化
+    //! @return 初期化済み
+    bool Init() override;
+
+    void Update() override;
+
+    void LateDraw() override;
+};
+
+}    // namespace PoittersPoint
